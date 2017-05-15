cf0f025ddfc35523388eaf473f65f7cd6fe3d6cd
Christopher Ryder <cryder9898@gmail.com>
2017-05-14 22:34:00 -0400

    added Header.js and Leaderboard.js connected with App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+var Leaderboard = require('./Leaderboard');
+
+function Header (props) {
+  return (
+    <div>
+      Header
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Leaderboard />
       </div>
     );
   }
---
+++ src/Leaderboard.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+class Leaderboard extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Leaderboard
+      </div>
+    )
+  }
+}
+
+module.exports = Leaderboard;
