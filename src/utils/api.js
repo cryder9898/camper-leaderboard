14bbfe0732b9aeaadb1595d0667e67f310ebbb88
Christopher Ryder <cryder9898@gmail.com>
2017-05-16 17:28:53 -0400

    spacing table columns

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,5 @@
 var axios = require('axios');
 
-function getRecentTopCampers () {
-  var uri = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
-  return axios.get(uri)
-    .then(function (response) {
-      return response.data;
-    });
-}
-
-function getTopAllTimeCampers () {
-  var uri = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
-
-  return axios.get(uri)
-  .then(function (response) {
-    return response.data;
-  });
-}
-
-
-
 module.exports = {
   fetchRecentTopCampers: function () {
     var uri = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
